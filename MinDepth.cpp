87f6b69b7c971704195fa102fe314adaef56b386
Arunaksha Talukdar <36398596+atlkdr@users.noreply.github.com>
2019-09-03 17:57:10 +0530

    Update MinDepth.cpp

--- MinDepth.cpp
+++ MinDepth.cpp
@@ -16,19 +16,32 @@ Node *newNode(int data)
     return (temp); 
 } 
 
+// Problem Comes into exsistence when one of left / right is null earler max would have worked fine
 int minDepth(Node* root){
-    if(root == NULL) return 0;
-    return 1+min(minDepth(root->left),minDepth(root->right));
+    if(root == NULL){
+        cout<<"ONLY ONCE"<<endl;
+        return 0;
+    }
+    if(root->left == NULL && root->right == NULL) return 1;
+    int left=root->left!=NULL?minDepth(root->left):INT_MAX;
+    int right=root->right!=NULL?minDepth(root->right):INT_MAX;
+    return 1+min(right,left);
+}
+// Changing the case check of NULL
+int minDepth2(Node* root){
+    if(root == NULL){
+        return INT_MAX;
+    }
+    if(root->left == NULL && root->right == NULL) return 1;
+    return 1+min(minDepth2(root->left),minDepth2(root->right));
 }
 
 int main()
 {
     Node *root     = newNode(1); 
     root->left     = newNode(2); 
-    root->right     = newNode(3); 
-    root->left->left = newNode(4); 
-    root->left->right = newNode(5); 
-    cout <<"The minimum depth of binary tree is : "<< minDepth(root); 
+    
+    cout <<"The minimum depth of binary tree is : "<< minDepth2(root); 
     return 0; 
     return 0;
 }
